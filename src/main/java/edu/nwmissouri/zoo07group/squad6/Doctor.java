f30aa8076a494406098b8d2fed67b709845768c0
manoj kota <kota.manoj8@gmail.com>
2021-11-02 16:36:07 -0500

    javadocs were added

--- src/main/java/edu/nwmissouri/zoo07group/squad6/Doctor.java
+++ src/main/java/edu/nwmissouri/zoo07group/squad6/Doctor.java
@@ -6,10 +6,15 @@ package edu.nwmissouri.zoo07group.squad6;
 
 /**
  *
- * @author S545411
+ * @author Manoj Kota
  */
 public class Doctor extends Human {
 
+    /**
+     *
+     * @param name
+     * defines name
+     */
     public Doctor(String name) {
         super(name);
     }
@@ -17,16 +22,35 @@ public class Doctor extends Human {
         DM,
         MBBS
     }
+
+    /**
+     *overriding role method
+     */
     @Override
     public void role() {
         System.out.printf("Doctor has to perform opearations");
     }
+
+    /**
+     * overriding experience method
+     */
     @Override
     public void experience() {
         System.out.printf("Doctor has 11 years of experience");
     }
+
+    /**
+     *overriding salary method
+     */
     @Override
     public void salary(){
         System.out.printf("Doctor salary would be 120K dollars");
     }
+    /**
+     * duties method
+     */
+     public void duties(){
+        System.out.printf("he advices duties on their and experince");
+    }
+    
 }
--- src/main/java/edu/nwmissouri/zoo07group/squad6/LegalAdvisor.java
+++ src/main/java/edu/nwmissouri/zoo07group/squad6/LegalAdvisor.java
@@ -6,23 +6,47 @@ package edu.nwmissouri.zoo07group.squad6;
 
 /**
  *
- * @author S545411
+ * @author Manoj Kota
  */
+
 public class LegalAdvisor extends Human{
+
+    /**
+     *
+     * @param name
+     * Legal advisor inheriting the human
+     */
     public LegalAdvisor (String name) {
         super(name);
     }
+
+    /**
+     * overriding role method
+     */
     @Override
     public void role() {
         System.out.printf("LegalAdvisor  has to advice the crew and staff on their actions");
     }
+
+    /**
+     * overriding experience method
+     */
     @Override
     public void experience() {
         System.out.printf("LegalAdvisor  has 21 years of experience");
     }
+
+    /**
+     * overriding salary method
+     */
     @Override
     public void salary(){
         System.out.printf("LegalAdvisor  salary would be 100K dollars");
+    }
+    public void operation()
+    {
+                System.out.printf("Doctor opearate for cardiac problems");
+
     }
 
     
