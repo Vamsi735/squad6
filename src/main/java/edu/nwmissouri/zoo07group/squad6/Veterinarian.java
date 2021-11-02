8917305a0d6ff30e5a0670c81a6a889cdce2df7e
akshaykrishna129 <akshaykrish129@gmail.com>
2021-11-02 17:29:03 -0500

    Added Veterinarian class and Registrar class

--- src/main/java/edu/nwmissouri/zoo07group/squad6/Veterinarian.java
+++ src/main/java/edu/nwmissouri/zoo07group/squad6/Veterinarian.java
@@ -6,8 +6,40 @@ package edu.nwmissouri.zoo07group.squad6;
 
 /**
  *
- * @author S545411
+ * @author Akshay krishna kintali
+ * Added Veterinarian  and their role is to assess the animals
  */
-public class Veterinarian implements Human{
+public class Veterinarian extends Human{
+   
+    public Veterinarian(String name) {
+        super(name);
+        }
+    @Override
+    //Added role of veternarian
+    public void role() {
+        System.out.printf("Examine animals to assess their health and diagnose problems");
+    }
+    @Override
+    //Added experience of veternarian
+    public void experience() {
+        System.out.printf("veternian have 20years of experience");
+    }
+    @Override
+    //Added salary of veternarian
+    public void salary(){
+        System.out.printf("veternarian avg salary is 80k dollars");
+    }
+    //Added vaccinate method  of veternarian
+    public void vaccinate(){
+        System.out.println("Systemvaccinate animals against diseases");
+    }
+    //Added advise method of veternarian
+    public void advise(){
+        System.out.println("Advise pet owners on nutrition, preventive healthcare and general care");
+        
+    }
+    
+   
+    
     
 }
