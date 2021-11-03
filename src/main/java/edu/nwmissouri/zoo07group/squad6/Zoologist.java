6d5dde48392a226d3b4e06f97f0050510621ccb5
vamsiareti <vamsiareti2020@gmail.com>
2021-11-02 19:22:30 -0500

    added my classes

--- src/main/java/edu/nwmissouri/zoo07group/squad6/Zoologist.java
+++ src/main/java/edu/nwmissouri/zoo07group/squad6/Zoologist.java
@@ -6,8 +6,47 @@ package edu.nwmissouri.zoo07group.squad6;
 
 /**
  *
- * @author S545411
+ * @author Vamsi Areti
  */
-public class Zoologist extends Human{
+public class Zoologist extends Human {
+
+    /**
+     *
+     * @param name
+     * defines name
+     */
+    public Zoologist(String name) {
+        super(name);
+    }
+
+    /**
+     *overriding role method
+     */
+    @Override
+    public void role() {
+        System.out.printf("Promoting the conservation of wildlife");
+    }
+
+    /**
+     * overriding experience method
+     */
+    @Override
+    public void experience() {
+        System.out.printf("Zoologist has 13 years of experience");
+    }
+
+    /**
+     *overriding salary method
+     */
+    @Override
+    public void salary(){
+        System.out.printf("Zoologist has salary around 120k dollars");
+    }
+    /**
+     * created method WorkKnowledge
+     */
+     public void workKnowledge(){
+        System.out.printf("I have more knowledge on Lion");
+    }
     
 }
