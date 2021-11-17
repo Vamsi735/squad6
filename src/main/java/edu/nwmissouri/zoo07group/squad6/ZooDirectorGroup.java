8fdfc3e117d8d7cd72087b6a25cd2a661966646a
vamsiareti <vamsiareti2020@gmail.com>
2021-11-16 21:27:51 -0600

    added group files

---
+++ src/main/java/edu/nwmissouri/zoo07group/squad6/ZooDirectorGroup.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo07group.squad6;
+import java.util.ArrayList;
+/**
+ *
+ * @author Vamsi krishna Areti (S5449902)
+ */
+public class ZooDirectorGroup {
+    private static ArrayList<ZooDirector> Zood;
+
+    public static int create() {
+        Zood = new ArrayList<>();
+
+        ZooDirector d = new ZooDirector("sub");
+        Zood.add(d);
+        Zood.add(new ZooDirector("curator"));
+        Zood.add(new ZooDirector("curator"));
+
+        return Zood.size();
+    }
+
+    public static void run() {
+        System.out.println("####################");
+        System.out.println("Hey - look at the Profession!");
+        Zood.forEach(ZooDirector -> {
+           
+            ZooDirector.newEvents();
+            ZooDirector.experience();
+            ZooDirector.role();
+            ZooDirector.salary();
+        });
+        System.out.println("My Zoodirector Attributes");
+        System.out.println("#################");
+    }
+
+    
+    
+}
